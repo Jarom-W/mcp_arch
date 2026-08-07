1d024b700771e07f319e918b4c6b32fb781d4a9d
Jarom-W <jaromwardwell@gmail.com>
2026-08-07 08:29:20 -0600

    run

--- run_mcp.sh
+++ run_mcp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 export MCP_READABLE_ROOTS="/home/jarom/Projects"
-export MCP_WRITABLE_ROOTS="/home/jarom/Projects/sandbox"
+export MCP_WRITABLE_ROOTS="/home/jarom/Projects/sandbox,/home/jarom/Projects/iA"
 
 exec /home/jarom/Projects/mcp_arch/target/debug/mcp_arch
